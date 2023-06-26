645b890db43cad254dcfb22596aad24511d179dc
slakovic <aladefm.o.a@gmail.com>
2023-06-26 22:54:46 +0100

    ES6 basics

---
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -0,0 +1,12 @@
+mport getBudgetObject from './7-getBudgetObject';
+
+export default function getFullBudgetObject(income, gdp, capita) {
+  const budget = getBudgetObject(income, gdp, capita);
+  const fullBudget = {
+    ...budget,
+    getIncomeInDollars: (income) => `$${income}`,
+    getIncomeInEuros: (income) => `${income} euros`,
+  };
+
+  return fullBudget;
+}
