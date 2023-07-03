c210ea0db4c4779f1c09640db84072571c5df098
slakovic <aladefm.o.a@gmail.com>
2023-07-03 20:26:36 +0100

    by ioc

---
+++ 0x03-ES6_data_manipulation/2-get_students_by_ioc.js
@@ -0,0 +1,7 @@
+/* eslint-disable */
+
+function getStudentsByLocation(students, city) {
+    return students.filter((student) => student.location === city);
+  }
+  
+  export default getStudentsByLocation;
