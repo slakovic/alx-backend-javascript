f9f14732a71985e480d219e255aa1b06a908ac83
slakovic <aladefm.o.a@gmail.com>
2023-07-03 20:47:59 +0100

    uniq items

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,15 @@
+/* eslint-disable */
+
+const updateUniqueItems = (items) => {
+    if (!(items instanceof Map)) {
+      throw new Error('Cannot process');
+    }
+    for (const [k, v] of items.entries()) {
+      if (v === 1) {
+        items.set(k, 100);
+      }
+    }
+    return items;
+  };
+  
+  export default updateUniqueItems;
