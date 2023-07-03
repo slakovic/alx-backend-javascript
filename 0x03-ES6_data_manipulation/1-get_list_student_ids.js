bd60130a3f89f090db28f0049a33fdc3b9c9d479
slakovic <aladefm.o.a@gmail.com>
2023-07-03 20:23:56 +0100

    student ids

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+
+const getListStudentIds = (listStudents) => {
+    let listStudentIds = [];
+    if (!(listStudents instanceof Array)) {
+      return listStudentIds;
+    }
+    listStudentIds = listStudents.map((student) => student.id);
+    return listStudentIds;
+  };
+  
+  export default getListStudentIds;
